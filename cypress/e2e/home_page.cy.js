b03cec3d215ce9f59817394061a58e0cbe8c62c3
Big-Throw <khinache.bilel@outlook.fr>
2023-09-12 23:57:38 +0200

    add new file

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -3,9 +3,10 @@ import User from "../support/user";
 let gender = "#gender";
 let firstName = "#first-name";
 let lastName = "#last-name";
-let message = "#message";
 let company = "#company";
 let phone = "#phone";
+let message_title = "#message-title";
+let message = "#message";
 
 let contact_form = "#contact-form";
 
@@ -15,108 +16,110 @@ let popin_success = ".popin-success";
 let popin_success_msg = "Le message a été envoyé.";
 let popin_error_msg = "Veuillez remplir tous les champs obligatoires.";
 
+let phone_value = "0102030405";
+let company_value = "Microsoft";
+let message_title_value = "Objet : tester le formulaire";
 let message_value = "Message de test";
-let phone_value = "0102030405"
-let company_value = "Microsoft"
 
 let user;
 
 describe("Test du formulaire", () => {
   beforeEach(() => {
     cy.visit("/");
-
+    
     user = new User();
     user.getRandomUser();
-
+    
     cy.log(user.id);
   });
-
+  
   it("Vérifie que le formulaire est invalide sans genre", () => {
     cy.get(firstName).type(user.firstname);
     cy.get(lastName).type(user.lastname);
     cy.get(message).type(message_value);
-
+    
     cy.get(contact_form).submit();
-
+    
     cy.get(popin).should("be.visible");
-
+    
     cy.get(popin_message).should("contain", popin_error_msg);
-
+    
     cy.get(popin_success).should("not.exist");
   });
-
+  
   it("Vérifie que le formulaire est invalide sans prénom", () => {
     cy.get(gender).select(user.gender);
     cy.get(lastName).type(user.lastname);
     cy.get(message).type(message_value);
-
+    
     cy.get(contact_form).submit();
-
+    
     cy.get(popin).should("be.visible");
-
+    
     cy.get(popin_message).should("contain", popin_error_msg);
-
+    
     cy.get(popin_success).should("not.exist");
   });
-
+  
   it("Vérifie que le formulaire est invalide sans nom", () => {
     cy.get(gender).select(user.gender);
     cy.get(firstName).type(user.lastname);
     cy.get(message).type(message_value);
-
+    
     cy.get(contact_form).submit();
-
+    
     cy.get(popin).should("be.visible");
-
+    
     cy.get(popin_message).should("contain", popin_error_msg);
-
+    
     cy.get(popin_success).should("not.exist");
   });
-
+  
   it("Vérifie que le formulaire est invalide sans message personnel", () => {
     cy.get(gender).select(user.gender);
     cy.get(firstName).type(user.firstname);
     cy.get(lastName).type(user.lastname);
-
+    
     cy.get(contact_form).submit();
-
+    
     cy.get(popin).should("be.visible");
-
+    
     cy.get(popin_message).should("contain", popin_error_msg);
-
+    
     cy.get(popin_success).should("not.exist");
   });
-
+  
   it("Vérifie que le formulaire est valide (sans champs optionels)", () => {
     cy.get(gender).select(user.gender);
     cy.get(firstName).type(user.firstname);
     cy.get(lastName).type(user.lastname);
     cy.get(message).type(message_value);
-
+    
     cy.get(contact_form).submit();
-
+    
     cy.get(popin).should("be.visible");
-
+    
     cy.get(popin_message).should("contain", popin_success_msg);
-
+    
     cy.get(popin_success).should("exist");
   });
-
+  
   it("Vérifie que le formulaire est valide (avec champs optionels)", () => {
     cy.get(gender).select(user.gender);
     cy.get(firstName).type(user.firstname);
     cy.get(lastName).type(user.lastname);
     cy.get(message).type(message_value);
-
+    
+    cy.get(message_title).type(message_title_value),
     cy.get(company).type(company_value);
     cy.get(phone).type(phone_value);
-
+    
     cy.get(contact_form).submit();
-
+    
     cy.get(popin).should("be.visible");
-
+    
     cy.get(popin_message).should("contain", popin_success_msg);
-
+    
     cy.get(popin_success).should("exist");
   });
 });
